30ad57af8e183606810f61210165b7a4d5269ee7
Lucas Garaglia <85807665+lucasgaraglia@users.noreply.github.com>
2023-08-22 13:35:45 -0300

    function verify_data in main.php

--- php/main.php
+++ php/main.php
@@ -1,15 +1,39 @@
 <!-- aca van a estar todas las funciones que vamos a utilizar en el sistema -->
 
-<!-- conexion a bdd con PDO (framework de php)-->
 <?php
-/* PDO('mysql:host=nombre_host;dbname=nombre_db;','user','password') */
-/* esto es una instancia de la clase PDO */
+/* !!!conexion a bdd con PDO (framework de php)!!! */
+
+    /* PDO('mysql:host=nombre_host;dbname=nombre_db;','user','password') */
+    /* esto es una instancia de la clase PDO */
 function connection(){
     $pdo = new PDO('mysql:host=localhost;dbname=inventory;','root','');
     return $pdo;
 }
+    /* -> significa que voy a usar un metodo de la instancia */
+    /* ejemplo insert sql */
+    /*$pdo->query("INSERT INTO category(category_name, category_address) VALUES('test','address test')");*/
 
-/* -> significa que voy a usar un metodo de la instancia */
-/* ejemplo insert sql */
-/*$pdo->query("INSERT INTO category(category_name, category_address) VALUES('test','address test')");*/
+
+
+
+/* funcion verificar datos formulario*/
+    // lleva como parametros el filtro aplicado (expresiones regulares) y el string
+function verify_data($filter, $string){
+    //la funcion preg_match tambien recibe esos dos parametros
+    if(preg_match("/^".$filter."$/", $string)){
+        // si el string cumple con las condiciones del filtro, devuelvo FALSE (que no hay error)
+        return false;
+    }else{
+        // si no cumple, devuelvo TRUE (hay error)
+        return true;
+    }
+}
+// ejemplo de uso
+// $name2 = "Crist2";
+// if(verify_data("[a-zA-Z]{4,20}",$name2)){
+//     echo "Please enter the right data.";
+// }
+
+
+
 ?>
